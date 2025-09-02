89c4480ed937487ca6fed77c2380a17e695b69db
Rx1m <goo.melyas@gmail.com>
2025-09-02 11:02:45 +0200

    Add script: 76 (User: 792090734831992886)

---
+++ scripts/792090734831992886/76.lua
@@ -0,0 +1,76 @@
+--[[
+Obfuscated with Hercules Obfuscator
+Original obfuscator by: zeusssz
+Repository: https://github.com/zeusssz/hercules-obfuscator
+License: Apache License 2.0
+
+Script: {'anti_debug': True, 'control_flow': True, 'minify': True, 'add_junk': True}
+Obfuscation Level: MAX
+Generated: 2025-09-02 09:02:43 UTC
+--]]
+
+
+-- Hercules Anti-Tamper Protection
+local function integrity_check()
+    local expected = "7e18107c109e4da5f29d1685676eb17f"
+    -- Runtime integrity verification
+    return true
+end
+if not integrity_check() then
+    error("Script integrity compromised")
+end
+
+local YyQ__4D1gtwwWK_1hpvaDKB3ZpTdVnVZNfM = function()
+local _ = tostring(false)
+local _ = string.len("")
+local _ = string.len("")
+local _ = math.random()
+local _ = tostring(false)
+local _ = math.random()
+local _ = table.concat({})
+local _ = math.random()
+local _ = table.concat({})
+local _ = tostring(false)
+local _ = table.concat({})
+local _ = math.random()
+local _ = type(nil)
+local _ = type(nil)
+local _ = tostring(false)
+local _ = string.len("")
+local _ = string.len("")
+local _ = table.concat({})
+local _ = tostring(false)
+local _ = tostring(false)
+local _ = math.random()
+local _ = table.concat({})
+local _ = type(nil)
+local _ = math.random()
+local _ = string.len("")
+local _ = type(nil)
+local _ = tostring(false)
+local _ = math.random()
+local _ = math.random()
+local _ = tostring(false)
+local _ = string.len("")
+local _ = tostring(false)
+local _ = tostring(false)
+local _ = table.concat({})
+local _ = string.len("")
+local _ = string.len("")
+local _ = type(nil)
+local _ = string.len("")
+local _ = type(nil)
+local _ = tostring(false)
+local _ = type(nil)
+local _ = math.random()
+print((string.char(0x68) .. string.char(0x69)))
+local _ = tostring(false)
+local _ = string.len("")
+local _ = type(nil)
+local _ = table.concat({})
+local _ = type(nil)
+local _ = type(nil)
+local _ = tostring(false)
+local _ = string.len("")
+end
+YyQ__4D1gtwwWK_1hpvaDKB3ZpTdVnVZNfM()

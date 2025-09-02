a2f165a85bd46000122969c79ad37d238d0215f4
Rx1m <goo.melyas@gmail.com>
2025-09-02 11:06:54 +0200

    Add script: 69 (User: 792090734831992886)

---
+++ scripts/792090734831992886/69.lua
@@ -0,0 +1,15 @@
+--[[
+Obfuscated with Hercules Obfuscator
+Original obfuscator by: zeusssz
+Repository: https://github.com/zeusssz/hercules-obfuscator
+License: Apache License 2.0
+
+Script: {'anti_debug': True, 'control_flow': True, 'minify': True, 'add_junk': True}
+Obfuscation Level: MEDIUM
+Generated: 2025-09-02 09:06:52 UTC
+--]]
+
+local K2FCHX4Yp0WjTS2T9vdy = function()
+print((string.char(104) .. string.char(105)))
+end
+K2FCHX4Yp0WjTS2T9vdy()
